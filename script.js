4094d527d165da5699cdd77240fb1f531d3cf99d
abel <abel_perez9@hotmail.com>
2023-03-14 16:03:25 -0500

    notes describing code

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+// ES6 Variables
 let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
+// ES6 Arrow Function
 const generateTarget = () => {
   return Math.floor(Math.random() * 10);
 };
@@ -9,7 +11,7 @@ const generateTarget = () => {
 const compareGuesses = (humanGuess, computerGuess, targetGuess) => {
   const humanDifference = Math.abs(targetGuess - humanGuess);
   const computerDifference = Math.abs(targetGuess - computerGuess);
-
+  // Conditional Statement
   if (humanGuess < 0 || humanGuess > 9) {
     alert("Next time pick a number between 0 & 9");
   }
@@ -20,6 +22,7 @@ const compareGuesses = (humanGuess, computerGuess, targetGuess) => {
   return false;
 };
 
+// Updates the global variables
 const updateScore = (winner) => {
   if (winner === "human") {
     humanScore++;
